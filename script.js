a776468a93c7fec6cef63f41f1f660c964fa92e1
HarshGautam-code <149058537+HarshGautam-code@users.noreply.github.com>
2026-02-25 14:28:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+
+// get the input field 
+const input = document.getElementById("fname");
+
+// run when the input loses focus 
+input.addEventListner("blur", function() {
+	input.value = input.value.toUppercase();
+});
